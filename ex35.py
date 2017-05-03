775587c98f58bb2c1a3fa055f1b6f2fe1ab42030
jobizness <jobizness87@gmail.com>
2017-05-02 20:07:32 -0400

    simple text adv
    newbie python

---
+++ ex35.py
@@ -0,0 +1,108 @@
+from sys import exit
+import random
+
+def gold_room():
+    print "This room is full of gold. How much do you take?"
+
+    choice = raw_input(">>")
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man learn to type a number.")
+
+    if how_much < 50:
+        print "Nice, you're not greedy. you win!"
+        exit(0)
+    else:
+        dead("You greedy bastard!")
+
+
+def bear_room():
+    print """
+    There is a bear here.
+    The bear has a bunch of honey.
+    The fat bear is in front of another door.
+    How are you going to move the bear?"""
+    bear_moved = False
+
+    while True:
+        choice = raw_input(">>")
+
+        if choice == "take honey":
+            dead("The bear looks at you then slaps your face off.")
+        elif choice == "taunt bear" and not bear_moved:
+            print "The bear has moved from the door. You can go through it now."
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+        elif choice == "open door" and bear_moved:
+            dice_room()
+        else:
+            print "I got no idea what that means."
+
+def cthulhu_room():
+    print "Here you see the great evil Cthulhu."
+    print "He, it, whatever stares at you and you go insane."
+    print "Do you flee for your life or eat your head?"
+
+    choice = raw_input(">>")
+
+    if "flee" in choice:
+        start()
+    elif "eat" or "head" in choice:
+        dead("Well that was tasty")
+    else:
+        cthulhu_room()
+
+
+def dead(why):
+    print why, "Good job!"
+    exit(0)
+
+def start():
+    print "You are in a dark room."
+    print "There is a door to your right and left."
+    print "Which one do you take?"
+
+    choice = raw_input(">>")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you starve.")
+
+
+def dice_roll():
+    print random.randint(2, 12)
+
+def spike_pit():
+    dead("A trap door opened and you fall into a pit of spikes.")
+
+def dice_room():
+    print "You've entered a room with a strange glow."
+    print "There is a table in the corner with some glowing dice."
+    print "There is only one door ahead."
+    print "Should you roll some dice?"
+
+
+    while True:
+        choice = raw_input(">>")
+
+        if choice == "Yes" or choice == "yes" or choice == "y":
+            dice_roll()
+            if dice_roll == 2 or dice_roll == 12:
+                print "You rolled a %d. Moving to next room."% dice_roll
+                gold_room()
+            elif dice_roll > 2 or dice_roll < 5:
+                print "You rolled a %d. Moving to next room." % dice_roll
+                cthulhu_room()
+            else:
+                print "You rolled a %d. Moving to next room."% dice_roll
+                start()
+        else:
+            print "You ignored the dice and went through the door."
+            spike_pit()
+
+start()
